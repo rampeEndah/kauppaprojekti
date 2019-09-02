6280e0a1f4ddb0ee4a175a9e618c05239e50ad46
Endah Rahmannudin <a1702578@myy.haaga-helia.fi>
2019-09-02 10:04:13 +0300

    ekat lisäykset

---
+++ github-testi/src/main/java/hh/swd20/github/testi/GithubTestiApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.github.testi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTestiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTestiApplication.class, args);
+	}
+
+}
